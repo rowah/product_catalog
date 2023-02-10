dcb91ca8ae4eadb7886d417027d7de9e38140882
Rowa <jrowah@gmail.com>
2023-02-10 22:51:49 +0300

    grants carts/creates for new user

--- lib/atula_web/router.ex
+++ lib/atula_web/router.ex
@@ -8,8 +8,33 @@ defmodule AtulaWeb.Router do
     plug :put_root_layout, {AtulaWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
+    plug :fetch_current_cart
   end
 
+  defp fetch_current_user(conn, _) do
+    if user_uuid = get_session(conn, :current_uuid) do
+      assign(conn, :current_uuid, user_uuid)
+    else
+      new_uuid = Ecto.UUID.generate()
+
+
+      conn
+      |> assign(:currents_uuid, new_uuid)
+      |> put_session(:currents_uuid, new_uuid)
+    end
+  end
+
+  alias Atula.ShoppingCart
+
+  defp fetch_current_cart(conn, _opts) do
+    if cart = ShoppingCart.get_cart_by_user_uuid(conn.assisgns.current_uuid) do
+      assign(conn, :cart, cart)
+    else
+      {:ok, new_cart} = ShoppingCart.create_cart(conn.assigns.current_uuid)
+      assign(conn, :cart, new_cart)
+    end
+  end
   pipeline :api do
     plug :accepts, ["json"]
   end
