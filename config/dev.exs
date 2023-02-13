27942138f223ce8593ce453bcf08c6ee7399a7bb
Rowa <jrowah@gmail.com>
2023-02-13 20:58:30 +0300

    installs tailwind

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :atula, AtulaWeb.Endpoint,
   secret_key_base: "hsN5Gjj5e4zZefq5PjJDf1VCSzwg6t+Q9h62DgcDhCYqSAy5ohueIp8SfjHRp3kb",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
